9da0a4efce80d0e5442098739164fdd2105e15ac
ximenes <xresende@gmail.com>
2018-09-25 16:56:31 -0300

    Rename hla docker launcher script

--- sirius-hla-docker.sh
+++ sirius-hla-docker.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-	echo 'Usage: docker-hla.sh AppName'
+	echo 'Usage: sirius-hla-docker.sh AppName'
 	echo 'Applications:'
-	echo '  sirius-hla-as-ps-launcher.py'
-	echo '  sirius-hla-as-ma-launcher.py'
-	echo '  sirius-hla-tb-ap-posang.py'
-	echo '  sirius-hla-ts-ap-posang.py'
-	echo '  sirius-hla-bo-ap-chromcorr.py'
-	echo '  sirius-hla-si-ap-chromcorr.py'
-	echo '  sirius-hla-bo-ap-tunecorr.py'
-	echo '  sirius-hla-si-ap-tunecorr.py'
-	echo '  sirius-hla-bo-ap-currlt.py'
-	echo '  sirius-hla-si-ap-currlt.py'
-	echo '  sirius-hla-bo-config-manager.py'
-	echo '  sirius-hla-si-ap-sofb.py'
+  echo '  as-ma-launcher.py'
+	echo '  as-ps-launcher.py'
+	echo '  tb-ap-posang.py'
+  echo '  bo-ap-chromcorr.py'
+  echo '  bo-ap-currlt.py'
+  echo '  bo-ap-tunecorr.py'
+  echo '  bo-ap-confmgr.py'
+  echo '  bo-ap-ramp.py'
+	echo '  ts-ap-posang.py'
+	echo '  si-ap-chromcorr.py'
+  echo '  si-ap-currlt.py'
+  echo '  si-ap-sofb.py'
+	echo '  si-ap-tunecorr.py'
 	exit 1
 fi
 
@@ -24,7 +25,7 @@ fi
 echo 'Running Docker'
 echo "VACA_PREFIX=$VACA_PREFIX"
 echo "EPICS_CA_ADDR_LIST=$EPICS_CA_ADDR_LIST"
-echo "HLA=$1"
+echo "HLA=sirius-hla-$1"
 
 docker run --network=host -it --rm -e DISPLAY=$DISPLAY -e VACA_PREFIX=$VACA_PREFIX -e EPICA_CA_AUTO_ADDR_LIST=$EPICS_CA_AUTO_ADDR_LIST -e EPICS_CA_ADDR_LIST="$EPICS_CA_ADDR_LIST" -v /tmp/.X11-unix:/tmp/.X11-unix lnlsfac/docker-hla /bin/bash -c $1
 
